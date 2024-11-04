c1080623ebc1d159349bca8f4a80a7c9a747a005
campo22 <miguelcampodiaz@gmail.com>
2024-11-03 22:37:50 -0500

    estructurando el proyecto

---
+++ src/main/java/com/sistema/examenes/sistemaexamenesbacken/SistemaExamenesBackenApplication.java
@@ -0,0 +1,13 @@
+package com.sistema.examenes.sistemaexamenesbacken;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaExamenesBackenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SistemaExamenesBackenApplication.class, args);
+    }
+
+}
